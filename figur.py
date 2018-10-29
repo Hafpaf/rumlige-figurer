86ad6e46f825e707e11b79bb15740c8d8973207f
hafpaf <haf@hafnium.me>
2018-10-29 11:19:07 +0100

    rename test.py to figur.py

--- figur.py
+++ figur.py
@@ -1,7 +1,9 @@
 
 
 import matplotlib.pyplot as plt
+import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-plt.show()
+plt.show() #starts the visual projector
