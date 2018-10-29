b1013b80975484cc6236d8f6db014ad3cb259a54
hafpaf <haf@hafnium.me>
2018-10-29 13:28:59 +0100

    add 2nd function

--- 2d_figur.py
+++ 2d_figur.py
@@ -2,7 +2,12 @@ import matplotlib.pylab as plt
 import numpy as np
 x = np.linspace(-np.pi, np.pi, 201)
 #plt.plot(x, np.sin(x))
+
+#1st function: f(x) = -1/4x +0.1*sin(3x)+2 , -2;0
 plt.plot(x, (-1/4*x + 0.1 * np.sin(3*x ) + 2))
+#2nd function: f(x) = -1/3x+0.1*sin(3x)+2 , 0;3
+plt.plot(x, -1/3*x+0.1*np.sin(3*x)+2)
+
 plt.xlabel('Angle [rad]')
 plt.ylabel('sin(x)')
 plt.axis('tight')
