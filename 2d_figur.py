f241020832540523bc2fcc026e393b8634dbdf8d
hafpaf <haf@hafnium.me>
2018-10-29 13:34:31 +0100

    add 3rd function

--- 2d_figur.py
+++ 2d_figur.py
@@ -5,10 +5,10 @@ x = np.linspace(-np.pi, np.pi, 201)
 
 #1st function: f(x) = -1/4x +0.1*sin(3x)+2 , -2;0
 plt.plot(x, (-1/4*x + 0.1 * np.sin(3*x ) + 2))
-#2nd function: f(x) = -1/3x+0.1*sin(3x)+2 , 0;3
+#2nd function: g(x) = -1/3x+0.1*sin(3x)+2 , 0;3
 plt.plot(x, -1/3*x+0.1*np.sin(3*x)+2)
-#3rd function:
-#plt.plot(x, )
+#3rd function: h(x)= -1/4x +0.05*cos(3x)+1.83556
+plt.plot(x, -1/4*x +0.05*np.cos(3*x)+1.83556)
 
 #labels
 plt.xlabel('Angle [rad]')
