58ee4325ed8bc5807a21ccfcc2e456baf2493f42
hafpaf <haf@hafnium.me>
2018-10-29 13:32:00 +0100

    add comments

--- 2d_figur.py
+++ 2d_figur.py
@@ -7,8 +7,12 @@ x = np.linspace(-np.pi, np.pi, 201)
 plt.plot(x, (-1/4*x + 0.1 * np.sin(3*x ) + 2))
 #2nd function: f(x) = -1/3x+0.1*sin(3x)+2 , 0;3
 plt.plot(x, -1/3*x+0.1*np.sin(3*x)+2)
+#3rd function:
+#plt.plot(x, )
 
+#labels
 plt.xlabel('Angle [rad]')
 plt.ylabel('sin(x)')
+
 plt.axis('tight')
-plt.show()
+plt.show() #display
