7ebd2ca166048a4316058135c2234f9611a25041
hafpaf <haf@hafnium.me>
2018-10-30 19:16:13 +0100

    comment definetion

--- 2d_figur.py
+++ 2d_figur.py
@@ -2,30 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #lenght of x axis
-x = np.linspace(-2, 7.25, 201)
+x = np.linspace(-2, 7.25)
+#y = np.linspace(0,10) #not needed
 #plt.plot(x, np.sin(x))
 
-def f1(t):
-    return (-1/4*x + 0.1 * np.sin(3*x ) + 2)
+#def f1(t):
+#    return (-1/4*x + 0.1 * np.sin(3*x ) + 2)
 t1 = np.arange(-2.0,0.0)
-plt.plot(f1(t1))
+#plt.plot(f1(t1))
 
-def f2(t):
-    return (-1/3*x+0.1*np.sin(3*x)+2)
-t2 = np.arange(0.0,3.0)
-plt.plot(f2(t2))
-
-def f3(t):
-    return (-1/4*x +0.05*np.cos(3*x)+1.83556)
-t3 = np.arange(3.0,7.16606)
-plt.plot(f3(t3))
+#def f2(t):
+#    return (-1/3*x+0.1*np.sin(3*x)+2)
+#t2 = np.arange(0.0,3.0)
+#plt.plot(f2(t2))
+#
+#def f3(t):
+#    return (-1/4*x +0.05*np.cos(3*x)+1.83556)
+#t3 = np.arange(3.0,7.16606)
+#plt.plot(f3(t3))
 
 #1st function: f(x) = -1/4x +0.1*sin(3x)+2 , -2;0
-#function1 = plt.plot(x, (-1/4*x + 0.1 * np.sin(3*x ) + 2))
+function1 = plt.plot(x, -1/4*x+0.1*np.sin(3*x)+2)
 #2nd function: g(x) = -1/3x+0.1*sin(3x)+2 , 0;3
-#plt.plot(x, -1/3*x+0.1*np.sin(3*x)+2)
+function2 = plt.plot(x, -1/3*x+0.1*np.sin(3*x)+2)
 #3rd function: h(x)= -1/4x +0.05*cos(3x)+1.83556 , 3;7.16606
-#plt.plot(x, -1/4*x +0.05*np.cos(3*x)+1.83556)
+function3 = plt.plot(x, -1/4*x +0.05*np.cos(3*x)+1.83556)
 
 #labels
 plt.xlabel('x-akse')
