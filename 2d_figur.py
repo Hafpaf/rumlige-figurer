2c14c2e94e6d636e642b3859fd0adfa9cb70273d
hafpaf <haf@hafnium.me>
2018-10-31 11:16:39 +0100

    add start and stop interval of functions

--- 2d_figur.py
+++ 2d_figur.py
@@ -3,8 +3,12 @@ import numpy as np
 
 #lenght of x axis
 x = np.linspace(-2, 7.25)
+xv = np.linspace(-2, 0)
+xz = np.linspace(0, 3)
+xc = np.linspace(3, 7.16606)
 #y = np.linspace(0,10) #not needed
 #plt.plot(x, np.sin(x))
+#np.meshgrid() #an addition to 3d figure
 
 '''
 def f1(t):
@@ -24,11 +28,11 @@ plt.plot(f3(t3))
 '''
 
 #1st function: f(x) = -1/4x +0.1*sin(3x)+2 , -2;0
-function1 = plt.plot(x, -1/4*x+0.1*np.sin(3*x)+2)
+function1 = plt.plot(xv, -1/4*xv+0.1*np.sin(3*xv)+2)
 #2nd function: g(x) = -1/3x+0.1*sin(3x)+2 , 0;3
-function2 = plt.plot(x, -1/3*x+0.1*np.sin(3*x)+2)
+function2 = plt.plot(xz, -1/3*xz+0.1*np.sin(3*xz)+2)
 #3rd function: h(x)= -1/4x +0.05*cos(3x)+1.83556 , 3;7.16606
-function3 = plt.plot(x, -1/4*x +0.05*np.cos(3*x)+1.83556)
+function3 = plt.plot(xc, -1/4*xc +0.05*np.cos(3*xc)+1.83556)
 
 #labels
 plt.xlabel('x-akse')
