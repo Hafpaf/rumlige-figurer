fc89cf1db418a253dcd2eed5d517ec734aca5eb3
hafpaf <haf@hafnium.me>
2018-11-02 09:04:34 +0100

    add integrate function, not workin yet

--- 3d_figur.py
+++ 3d_figur.py
@@ -13,15 +13,17 @@ xv = np.linspace(-2, 0)
 xz = np.linspace(0, 3)
 xc = np.linspace(3, 7.16606)
 #np.meshgrid() #an addition to 3d figure
-
+'''
 #1st function: f(x) = -1/4x +0.1*sin(3x)+2 , -2;0
 function1 = plt.plot(xv, -1/4*xv+0.1*np.sin(3*xv)+2)
 #2nd function: g(x) = -1/3x+0.1*sin(3x)+2 , 0;3
 function2 = plt.plot(xz, -1/3*xz+0.1*np.sin(3*xz)+2)
 #3rd function: h(x)= -1/4x +0.05*cos(3x)+1.83556 , 3;7.16606
 function3 = plt.plot(xc, -1/4*xc +0.05*np.cos(3*xc)+1.83556)
+'''
 
-#F(x)=integrate
+#funktion
+#F(x)=integrate.quad(((-1/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
 
 #labels
 plt.xlabel('x-akse')
