f44bc38b0a523906ea60ab96ad7ee0d5b2c7e59e
hafpaf <haf@hafnium.me>
2018-11-02 11:56:53 +0100

    add if statement to sort out

--- integral_test.py
+++ integral_test.py
@@ -1,13 +1,22 @@
 import scipy.integrate as integrate
 import scipy.special as special
 import numpy as np
-'''
-result = integrate.quad(lambda x: special.jv(2.5,x), 0, 4.5)
-print(result)
 
+#result = integrate.quad(lambda x: special.jv(2.5,x), 0, 4.5)
+#print(result)
+
+resultz = integrate.quad(lambda x: ((-1/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
+print(resultz)
+for result in resultz:
+    if result > 0.1:
+        print(result)
 #funktion
-F(x)=integrate.quad(((-1/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
+#F(x)=integrate.quad(((-1/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
 '''
 #funktion
-resultx = integrate.quad(lambda x: ((-1/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
-print(resultx)
+def function1():
+    resultz = integrate.quad(lambda x: ((-1/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
+    Fz = np.pi*resultz
+    return Fz
+print(function1())
+'''
