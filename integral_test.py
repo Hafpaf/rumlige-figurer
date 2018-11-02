ff7c41cbb3ad02a030d61f9f2745658151ea8214
hafpaf <haf@hafnium.me>
2018-11-02 12:18:18 +0100

    comment if statement and print 1st number given

--- integral_test.py
+++ integral_test.py
@@ -7,24 +7,31 @@ import numpy as np
 
 #1st function
 resultz = integrate.quad(lambda x: (((-1)/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
-#print(resultz)
+print("1:", resultz[0])
+'''
 for result in resultz:
     if result > 0.0:
         print("1: ", result)
+'''
 #2nd function
 resultx = integrate.quad(lambda x: (((-1)/3*x+0.1*np.sin(3*x)+2)**2), 0, 3)
+print("2:", resultx[0])
+'''
 for result in resultx:
     if result > 0.0:
         print("2: ", result)
+'''
 #3rd function
 resultc = integrate.quad(lambda x: (((-1)/4*x+0.5*np.cos(3*x)+1.83556)**2), 3, 7.16606)
+print("3:", resultc[0])
+'''
 for result in resultc:
     if result in resultc:
         if result > 0.0:
             print("3 :", result)
-
 '''
 
+'''
 #funktion
 def function():
     resultz = integrate.quad(lambda x: ((-1/4*x+0.1*np.sin(3*x)+2)**2), -2, 0)
